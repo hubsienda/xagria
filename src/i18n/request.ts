f664014271a878360fbe9b5215b11bdfdcb14546
hubsienda <hub@siendaweblines.com>
2026-04-13 15:38:28 +0200

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,12 @@
-import { getRequestConfig } from 'next-intl/server';
-import { routing } from './routing';
+import {getRequestConfig} from 'next-intl/server';
+import {hasLocale} from 'next-intl';
+import {routing} from './routing';
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
-
-  if (!locale || !routing.locales.includes(locale as any)) {
-    locale = routing.defaultLocale;
-  }
+export default getRequestConfig(async ({requestLocale}) => {
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale;
 
   return {
     locale,
