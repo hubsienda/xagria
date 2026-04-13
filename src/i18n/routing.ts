b6047bddf627775bf31fa4c6cee6d06e3796a9c0
hubsienda <hub@siendaweblines.com>
2026-04-13 14:40:51 +0200

    Create routing.ts

---
+++ src/i18n/routing.ts
@@ -0,0 +1,17 @@
+import {defineRouting} from 'next-intl/routing';
+import {createNavigation} from 'next-intl/navigation';
+
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ['en', 'es', 'it'],
+
+  // Used when no locale matches
+  defaultLocale: 'en',
+  
+  // Only show the locale in the URL when it's not the default
+  localePrefix: 'as-needed'
+});
+
+// Lightweight wrappers around Next.js navigation APIs
+export const {Link, redirect, usePathname, useRouter, getPathname} =
+  createNavigation(routing);
