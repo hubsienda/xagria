e88ef629195dd2891ae63bb4f9ca902c8449b138
hubsienda <hub@siendaweblines.com>
2026-04-13 15:35:20 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Config options here
+  reactStrictMode: true
 };
 
 export default withNextIntl(nextConfig);
