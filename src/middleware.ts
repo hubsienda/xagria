4880535116f944fe1b1dec6badf82b37d911297f
hubsienda <hub@siendaweblines.com>
2026-04-13 15:37:24 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
+import {routing} from './src/i18n/routing';
 
 export default createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en|es|it)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|trpc|_next|_vercel|.*\\..*).*)']
 };
