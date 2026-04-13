3a1bf04077cc2cc97f7c15a31303e817b7f5484f
hubsienda <hub@siendaweblines.com>
2026-04-13 14:46:47 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
+import {routing} from './i18n/routing';
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'es', 'it'],
-
-  // Used when no locale matches
-  defaultLocale: 'en',
-  
-  // Optional: removes the prefix for the default locale (xagria.com instead of xagria.com/en)
-  localePrefix: 'as-needed'
-});
+export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(en|es|it)/:path*']
+  matcher: ['/', '/(en|es|it)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
 };
