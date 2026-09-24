9db081667062526ffe07d407981cea13771b356c
hubsienda <hub@siendaweblines.com>
2026-09-24 17:42:07 +0200

    Use Eurostat reporter code for Greece

--- src/lib/trade/reporters.ts
+++ src/lib/trade/reporters.ts
@@ -3,7 +3,7 @@ import type {ReporterMarket} from './types';
 export const REPORTER_MARKETS: ReporterMarket[] = [
   ['AT', 'Austria'], ['BE', 'Belgium'], ['BG', 'Bulgaria'], ['HR', 'Croatia'], ['CY', 'Cyprus'],
   ['CZ', 'Czechia'], ['DK', 'Denmark'], ['EE', 'Estonia'], ['FI', 'Finland'], ['FR', 'France'],
-  ['DE', 'Germany'], ['GR', 'Greece'], ['HU', 'Hungary'], ['IE', 'Ireland'], ['IT', 'Italy'],
+  ['DE', 'Germany'], ['EL', 'Greece'], ['HU', 'Hungary'], ['IE', 'Ireland'], ['IT', 'Italy'],
   ['LV', 'Latvia'], ['LT', 'Lithuania'], ['LU', 'Luxembourg'], ['MT', 'Malta'], ['NL', 'Netherlands'],
   ['PL', 'Poland'], ['PT', 'Portugal'], ['RO', 'Romania'], ['SK', 'Slovakia'], ['SI', 'Slovenia'],
   ['ES', 'Spain'], ['SE', 'Sweden'],
